0e7fdcf9c5515e633b231c38309736843e428c9d
Harikesh-14 <68147228+Harikesh-14@users.noreply.github.com>
2024-03-25 23:35:56 +0530

    Working search functionality has been implemented

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
-// use std::env;
+use std::fs;
+use std::error::Error;
 
 pub struct Config {
     pub operation: String,
@@ -22,12 +23,65 @@ impl Config {
     }
 }
 
-pub fn run (config: Config) -> Result<(), Box<dyn std::error::Error>> {
-    println!("Operation: {}", config.operation);
-    println!("Expression: {}", config.expression);
-    println!("Filename: {}", config.filename);
-    println!("Is sensitive: {}", config.is_sensitive);
+pub fn run (config: Config) -> Result<(), Box<dyn Error>> {
+    let content = fs::read_to_string(&config.filename)?;
+    if config.operation == "search" {
+        println!("Searching for {}", config.expression);
+        // implementing the searching function
+        let search_res = search_word(&config.expression, &content, &config.is_sensitive);
+        // printing the result
+        for line in search_res {
+            println!("{}", line)
+        }
+    } else if config.operation == "replace" {
+        println!("Replacing {} with {}", config.expression, config.filename);
+    } else {
+        println!("Invalid operation");        
+    }
 
     Ok(())
-    
+}
+
+pub fn search_word<'a> (word: &str, content: &'a str, is_sensitive: &str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+    if is_sensitive == "true" {
+        for line in content.lines() {
+            if line.contains(word) {
+                result.push(line.trim())
+            }
+        }
+    } else if is_sensitive == "false" {
+        let word = word.to_lowercase();
+        for line in content.lines() {
+            if line.to_lowercase().contains(&word) {
+                result.push(line.trim())
+            }
+        }
+    } else {
+        eprintln!("Enter either true or false");
+    }
+
+    result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive_searching_function() {
+        let word = "om";
+        let content = "Om, Namah, Shivaay\nomicron";
+        let is_sensitive = "true";
+        assert_eq!(vec!["Om, Namah, Shivaay"], search_word(word, content, is_sensitive));
+    }
+
+    #[test]
+    fn case_insensitive_searching_function() {
+        let word = "om";
+        let content = "Om, Namah, Shivaay\nom, namah, shivaay";
+        let is_sensitive = "false";
+        assert_eq!(vec!["om, namah, shivaay"], search_word(word, content, is_sensitive));
+    }
+
 }
